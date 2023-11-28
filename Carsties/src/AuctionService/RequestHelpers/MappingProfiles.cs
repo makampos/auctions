fe6195016bbdd65993976e509b969d3d816850bc
makampos <matheus.campos@edu.unifil.br>
2023-11-28 16:07:27 -0300

    adding Dtos and mappings using AutoMapper library

---
+++ Carsties/src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+            CreateMap<Item, AuctionDto>();
+            CreateMap<CreateAuctionDto, Auction>()
+                .ForMember(x => x.Item, y
+                    => y.MapFrom(z => z));
+            CreateMap<CreateAuctionDto, Item>();
+        }
+    }
+}
